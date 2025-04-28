6fe2b00cb2ed828aa2cd0ea54d9c4d26235531e8
Soeler <k.a.s@hotmail.de>
2025-04-28 23:46:59 +0200

    kontakt and fancy stuff

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 window.addEventListener('load', () => {
     const loader = document.getElementById('loader');
     loader.style.display = 'none';
+    const container = document.querySelector('.icon-links');
+    if (container) {
+        const scrollWidth = container.scrollWidth;
+        const visibleWidth = container.clientWidth;
+        const scrollTo = (scrollWidth - visibleWidth) / 2;
+        container.scrollLeft = scrollTo;
+    }
 });
 
 // Twitch Streamtitel laden (vereinfachte Version)
