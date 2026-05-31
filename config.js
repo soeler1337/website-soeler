7e48ce3a604fc2d0a80fd4bbe96e5297624318d7
Soeler <k.a.s@hotmail.de>
2026-06-01 00:58:53 +0200

    fix: bio text, layout order, shorts pills, icon strip reorder
    - Update bio text to final version (TTRPG-Rabbithole, Actual Play etc.)
    - Move Ueber mich + Im Fokus above stream so they are visible on load
    - Simplify shorts pills to 3: soelers_ecke TikTok first, then soeler1337
    - Reorder icon strip: Twitch, soelers_ecke YT, Loredumb, soeler1337 YT...
    - Remove TaleWeaver from icon strip, clean up icon labels

--- config.js
+++ config.js
@@ -1,34 +1,39 @@
 const SITE_CONFIG = {
   icons: [
     {
+      name: "soeler1337",
+      url: "https://www.twitch.tv/soeler1337",
+      iconPath: "icons/twitch.png"
+    },
+    {
+      name: "Soelers Ecke",
+      url: "https://www.youtube.com/@soelers_ecke",
+      iconPath: "icons/youtube.png"
+    },
+    {
       name: "Loredumb Podcast",
       url: "https://www.loredumb.com",
       iconPath: "icons/podcast.png"
     },
     {
-      name: "Pixelart Galerie",
-      url: "https://www.pixilart.com/soeler/gallery",
-      iconPath: "icons/pixelart.png"
-    },
-    {
-      name: "Discord SoelerLand",
-      url: "https://discord.gg/UDwEWXBc4z",
-      iconPath: "icons/discord.png"
-    },
-    {
       name: "soeler1337",
-      url: "https://www.instagram.com/soeler1337",
-      iconPath: "icons/instagram.png"
+      url: "https://youtube.com/@soeler1337",
+      iconPath: "icons/youtube.png"
+    },
+    {
+      name: "Soeler Art",
+      url: "https://www.youtube.com/@soelerartger",
+      iconPath: "icons/youtube.png"
+    },
+    {
+      name: "soelers_ecke",
+      url: "https://www.tiktok.com/@soelers_ecke",
+      iconPath: "icons/tiktok.png"
     },
     {
       name: "soelers_ecke",
       url: "https://www.instagram.com/soelers_ecke",
       iconPath: "icons/instagram.png"
-    },
-    {
-      name: "soelers-ecke",
-      url: "https://www.tiktok.com/@soelers_ecke",
-      iconPath: "icons/tiktok.png"
     },
     {
       name: "soeler1337",
@@ -37,23 +42,18 @@ const SITE_CONFIG = {
     },
     {
       name: "soeler1337",
-      url: "https://www.twitch.tv/soeler1337",
-      iconPath: "icons/twitch.png"
+      url: "https://www.instagram.com/soeler1337",
+      iconPath: "icons/instagram.png"
     },
     {
-      name: "soeler1337",
-      url: "https://youtube.com/@soeler1337",
-      iconPath: "icons/youtube.png"
+      name: "Discord",
+      url: "https://discord.gg/UDwEWXBc4z",
+      iconPath: "icons/discord.png"
     },
     {
-      name: "Soeler Art - Deutsch",
-      url: "https://www.youtube.com/@soelerartger",
-      iconPath: "icons/youtube.png"
-    },
-    {
-      name: "soelers_ecke",
-      url: "https://www.youtube.com/@soelers_ecke",
-      iconPath: "icons/youtube.png"
+      name: "Pixelart",
+      url: "https://www.pixilart.com/soeler/gallery",
+      iconPath: "icons/pixelart.png"
     },
     {
       name: "Donation",
@@ -61,7 +61,7 @@ const SITE_CONFIG = {
       iconPath: "icons/donation.png"
     },
     {
-      name: "Support via Ko-Fi",
+      name: "Ko-Fi",
       url: "https://ko-fi.com/soeler",
       iconPath: "icons/kofi.png"
     }
